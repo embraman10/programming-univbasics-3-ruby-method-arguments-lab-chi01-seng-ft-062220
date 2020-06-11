c47ae730bb5d153253becb3e0e3137ef0da3992d
Emily Braman <embraman10@gmail.com>
2020-06-11 21:52:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,17 @@ def introduction
 end
 introduction(Emily)
 
+def introduction_with_language
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language(Emily, Ruby)
 
+def introduction_with_language_optional (name, FlatIron)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+introduction_with_language_optional(Emily)
+
+introduction_with_language_optional(Emily, Ruby)
 
 
 
