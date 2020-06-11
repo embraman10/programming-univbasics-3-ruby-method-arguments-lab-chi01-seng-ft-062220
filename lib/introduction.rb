ea420ff76123581fadc0af70ef6241e5b24be19b
Emily Braman <embraman10@gmail.com>
2020-06-11 21:51:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,23 @@
-# Write your code here. If you get stuck, at the very bottom of this lab we've included
-# the answers. You should ONLY check those answers if you get totally stuck. The important
-# thing is NOT to "complete labs," the important thing is to learn the skills you need
-# in order to complete the rest of this course and be ready for the next course!
+def introduction
+  puts "Hi my name is #{name}"
+end
+introduction(Emily)
+
+def introduction_with_language
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language(Emily, Ruby)
+
+def introduction_with_language_optional (name, FlatIron)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+introduction_with_language_optional(Emily)
+
+introduction_with_language_optional(Emily, Ruby)
+
+
+
+
 
 
 
